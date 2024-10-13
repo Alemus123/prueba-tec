a51faedfec4752a07ce29801a2f93918dbea60ff
Alejandra Lemus <johle1.1996@gmail.com>
2024-10-13 17:29:29 -0600

    first commit 2

---
+++ dotnet-registration-api/Controllers/UsersController.cs
@@ -0,0 +1,123 @@
+﻿using dotnet_registration_api.Data.Entities;
+using dotnet_registration_api.Data.Models;
+using dotnet_registration_api.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace dotnet_registration_api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private readonly UserService _userService;
+        public UsersController(UserService userService)
+        {
+            _userService = userService;
+        }
+        [HttpPost("login")]
+        public async Task<ActionResult<User>> Login([FromBody] LoginRequest model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            try
+            {
+                await _userService.Login(model);
+                return StatusCode(200, $"Login Exitoso");
+            }
+
+            catch (Exception ex)
+            {
+                return StatusCode(500, $"Internal server error: {ex.Message}");
+            }
+        }
+        [HttpPost("register")]
+        public async Task<ActionResult<User>> Register([FromBody] RegisterRequest model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            try
+            {
+
+                var user = await _userService.Register(model);
+
+                return user;
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, $"Internal server error: {ex.Message}");
+            }
+        }
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<User>>> GetAll()
+        {
+            try
+            {
+
+                var users = await _userService.GetAll();
+
+                return users;
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, $"Internal server error: {ex.Message}");
+            }
+        }
+        [HttpGet("{id}")]
+        public async Task<ActionResult<User>> GetById(int id)
+        {
+
+            try
+            {
+                var user = await _userService.GetById(id);
+
+                return user;
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, $"Internal server error: {ex.Message}");
+            }
+        }
+        [HttpPut("{id}")]
+        public async Task<ActionResult<User>> Update(int id, [FromBody] UpdateRequest model)
+        {
+
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            try
+            {
+                var user = await _userService.Update(id, model);
+
+                return  user;
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, $"Internal server error: {ex.Message}");
+            };
+        }
+        [HttpDelete("{id}")]
+        public async Task<ActionResult> Delete(int id)
+        {
+            try
+            {
+
+                await _userService.Delete(id);
+
+                return StatusCode(204, $"Usuario Eliminado");
+
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, $"Internal server error: {ex.Message}");
+            }
+        }
+    }
+}
---
+++ dotnet-registration-api/Services/UserService.cs
@@ -0,0 +1,159 @@
+﻿using dotnet_registration_api.Data;
+using dotnet_registration_api.Data.Entities;
+using dotnet_registration_api.Data.Models;
+using dotnet_registration_api.Data.Repositories;
+using dotnet_registration_api.Helpers;
+using Mapster;
+using Microsoft.Win32;
+using System.Text;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace dotnet_registration_api.Services
+{
+    public class UserService
+    {
+        private readonly UserRepository _userRepository;
+        private readonly UserDataContext _context;
+        public UserService(UserRepository userRepository , UserDataContext context)
+        {
+            _userRepository = userRepository;
+            _context = context;
+        }
+        public async Task<List<User>> GetAll()
+        {
+            return await _userRepository.GetAllUsers();
+        }
+        public async Task<User> GetById(int id)
+        {
+            var user = await _userRepository.GetUserById(id);
+
+            if (user == null)
+            {
+                throw new KeyNotFoundException("User not found");
+            }
+
+            return user;
+        }
+
+        public async Task<User> Login(LoginRequest login)
+        {
+
+            var hashedPassword = HashPassword(login.Password);
+
+            var user = new User
+            {
+                Username = login.Username,
+                PasswordHash = hashedPassword
+            };
+
+
+           var userLog =  await _userRepository.GetUserByUsernameAndPassword(user.Username , user.PasswordHash);
+
+            if (userLog != null)
+            {
+                return userLog;
+            }
+            else
+            {
+                throw new UnauthorizedAccessException("Invalid username or password");
+            }
+
+        }
+        public async Task<User> Register(RegisterRequest register)
+        {
+            var user = new User
+            {
+                Username = register.Username,
+                PasswordHash = HashPassword(register.Password), 
+                LastName = register.LastName,
+                FirstName = register.FirstName
+            };
+
+          
+            await _context.Users.AddAsync(user);
+
+        
+            await _context.SaveChangesAsync();
+
+            return user; 
+        }
+        public async Task<User> Update(int id, UpdateRequest updateRequest)
+        {
+            var user = await _userRepository.GetUserById(id);
+
+            if (user == null)
+            {
+                throw new KeyNotFoundException("User not found");
+            }
+
+            if (!string.IsNullOrWhiteSpace(updateRequest.FirstName))
+            {
+                user.FirstName = updateRequest.FirstName;
+            }
+
+            if (!string.IsNullOrWhiteSpace(updateRequest.LastName))
+            {
+                user.LastName = updateRequest.LastName;
+            }
+
+            if (!string.IsNullOrWhiteSpace(updateRequest.Username))
+            {
+                user.Username = updateRequest.Username;
+            }
+
+            if (!string.IsNullOrWhiteSpace(updateRequest.NewPassword))
+            {
+                if (user.PasswordHash != HashPassword(updateRequest.OldPassword))
+                {
+                    throw new UnauthorizedAccessException("Old password is incorrect");
+                }
+
+                user.PasswordHash = HashPassword(updateRequest.NewPassword);
+            }
+
+       
+            await _userRepository.UpdateUser(user);
+
+            return user;
+        }
+
+
+
+        private string HashPassword(string password)
+    {
+        
+        if (string.IsNullOrWhiteSpace(password))
+        {
+            throw new ArgumentException("Password cannot be null or empty.", nameof(password));
+        }
+
+        using (var sha256 = SHA256.Create())
+        {
+           
+            var passwordBytes = Encoding.UTF8.GetBytes(password);
+
+            var hashedBytes = sha256.ComputeHash(passwordBytes);
+
+            var hashedPassword = BitConverter.ToString(hashedBytes).Replace("-", "").ToLowerInvariant();
+
+            return hashedPassword; 
+        }
+    }
+
+
+    public async Task Delete(int id)
+        {
+            var user = await _userRepository.GetUserById(id);
+
+            if (user == null)
+            {
+                throw new KeyNotFoundException("User not found");
+            }
+
+            await _userRepository.DeleteUser(id);
+    
+        }
+
+    }
+}
